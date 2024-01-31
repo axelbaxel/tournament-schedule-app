a499c653b1dd67e49fa9b0f19bf2d51fe7ae3086
Axel Boix <axbo@computas.com>
2024-01-31 10:50:20 +0100

    register on client

--- app/add-tournament/page.tsx
+++ app/add-tournament/page.tsx
@@ -1,24 +1,11 @@
+import { RegistrationForm } from "./registration-form"
+
 export default function AddTournamentPage() {
-    const handleSubmit = (event: React.FormEvent) => {
-        event.preventDefault();
-        // Add logic to handle form submission here
-    };
 
     return (
         <>
             <h1>Add Tournament</h1>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="name">Name:</label>
-                <input type="text" id="name" name="name" />
-
-                <label htmlFor="date">Date:</label>
-                <input type="date" id="date" name="date" />
-
-                <label htmlFor="location">Location:</label>
-                <input type="text" id="location" name="location" />
-
-                <button type="submit">Submit</button>
-            </form>
+            <RegistrationForm />
         </>
     );
 }
